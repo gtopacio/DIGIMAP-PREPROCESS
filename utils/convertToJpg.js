5a1176bcbd505ad29a5a255df4f918d2336002f3
gtopacio <user@DESKTOP-MOMMK5K.localdomain>
2022-06-16 21:10:08 +0800

    EXIF

--- utils/convertToJpg.js
+++ utils/convertToJpg.js
@@ -1,6 +1,6 @@
 const sharp = require('sharp');
 module.exports = async function convertToJpg(path, mimetype){
     let newPath = path + "_jpg";
-    await sharp(path).toFormat("jpeg").jpeg({ force: true, mozjpeg: true }).toFile(newPath);
+    await sharp(path).toFormat("jpeg").jpeg({ force: true, mozjpeg: true }).withMetadata().toFile(newPath);
     return newPath;
 }

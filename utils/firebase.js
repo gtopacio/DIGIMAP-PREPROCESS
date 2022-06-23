9d0ef1c99396a565e68e46f4d74821d386ff5111
gtopacio <user@DESKTOP-MOMMK5K.localdomain>
2022-06-23 19:39:32 +0800

    Firebase

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,7 +1,13 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 const { getStorage } = require("firebase-admin/storage");
-const creds = require('../creds/firebaseAdmin.json');
+require('dotenv').config();
+
+const creds = {
+    "projectId": process.env.FIREBASE_PROJECT_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 const app = initializeApp({credential: cert(creds)});
 const db = getFirestore(app);
